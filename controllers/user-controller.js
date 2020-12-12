b14ee907da7a7eefd26f82cdf5627d4582721460
ginomaglaque <ginomaglaque@gmail.com>
2020-12-12 10:35:17 -0800

    populate thoughts

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -5,6 +5,11 @@ const userController = {
     // get all users
     getAllUser(req, res){
         User.find({})
+            .populate({
+                path: 'thoughts',
+                select: '-__v'
+            })
+            .select('-__v')
             .then(dbUserData => res.json(dbUserData))
             .catch(err => {
                 console.log(err);
@@ -15,6 +20,11 @@ const userController = {
     // get one user by id
     getUserById({ params }, res) {
         User.findOne({_id: params.id})
+            .populate({
+                path: 'thoughts',
+                select: '-__v'
+            })
+            .select('-__v')
             .then(dbUserData => res.json(dbUserData))
             .catch(err => {
                 console.log(err);
