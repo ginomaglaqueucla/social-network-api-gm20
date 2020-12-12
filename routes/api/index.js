64fb477326f729ae48e381818318409c86ce8758
ginomaglaque <ginomaglaque@gmail.com>
2020-12-12 09:36:13 -0800

    user api

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 // const commentRoutes = require('./comment-routes');
-const pizzaRoutes = require('./user-routes.js');
+const userRoutes = require('./user-routes.js');
 
 // router.use('/comments', commentRoutes);
 router.use('/user', userRoutes);
